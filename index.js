6fbbd1bc7aab107d07a7fba2d7e1d87ebd577feb
Aya Ramirez <ayaramz@gmail.com>
2022-04-27 19:21:06 -0400

    commit tree fd9344c5066c61bddfe539e98dac1e468a5b1dd9

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// const random = Math.random(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
+const random = Math.floor(Math.random() * 10)
+
+const num3 = 8;
+const num4 = 2;
+
+const mod = num3/num4;
+
+const max = Math.max(20);
